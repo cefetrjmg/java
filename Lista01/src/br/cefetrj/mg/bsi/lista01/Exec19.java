803c44b2613d8da9f0098883d40e99fbdad1184a
cristian <cristianmadeira1106@gmail.com>
2019-08-27 14:58:34 -0300

    First commit

---
+++ Lista01/src/br/cefetrj/mg/bsi/lista01/Exec19.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetrj.mg.bsi.lista01;
+
+import java.util.Random;
+
+/**
+ *
+ * @author cristian
+ */
+public class Exec19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Random r =new Random();
+    }
+    
+}
