d4f3d7b80fd5e5a21af6a43f51fffb53f2b185a9
aluno-2 <aluno-2@PC-64611-A314.CEFET.BR>
2019-08-28 18:03:31 -0300

    Primeiro commit do exercício 20.

---
+++ Lista01/src/br/cefetrj/mg/bsi/lista01/model/Exec20Model.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetrj.mg.bsi.lista01.model;
+
+/**
+ *
+ * @author aluno-2
+ */
+public class Exec20Model {
+    
+}
