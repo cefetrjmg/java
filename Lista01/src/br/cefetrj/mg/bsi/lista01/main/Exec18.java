8aa04cc6670825eadf4ce5749de21d2766419033
aluno-8 <aluno-8@PC-77159-L8.CEFET.BR>
2019-08-30 20:22:48 -0300

    Exercício 18 criado.

---
+++ Lista01/src/br/cefetrj/mg/bsi/lista01/main/Exec18.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetrj.mg.bsi.lista01.main;
+
+/**
+ *
+ * @author aluno-8
+ */
+public class Exec18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
