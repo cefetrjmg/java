06ce24a0cb2bd9948d3b79907dd0061a99bd0f8d
aluno-8 <aluno-8@PC-77159-L8.CEFET.BR>
2019-08-30 20:41:26 -0300

    pacote main renomeado para view.

--- Lista01/src/br/cefetrj/mg/bsi/lista01/view/Exec18.java
+++ Lista01/src/br/cefetrj/mg/bsi/lista01/view/Exec18.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.cefetrj.mg.bsi.lista01.main;
+package br.cefetrj.mg.bsi.lista01.view;
 
 /**
  *
--- Lista01/src/br/cefetrj/mg/bsi/lista01/view/Exec20.java
+++ Lista01/src/br/cefetrj/mg/bsi/lista01/view/Exec20.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.cefetrj.mg.bsi.lista01.main;
+package br.cefetrj.mg.bsi.lista01.view;
 
 import br.cefetrj.mg.bsi.lista01.model.Exec20Model;
 import br.cefetrj.mg.bsi.lista01.dao.Exec20DAO;
