7223e3aedfdfa28fbeef7e5424e0230f1af93340
Cristian Madeira deSouza Pereira <cristianpereira@tic.ufrj.br>
2019-09-08 18:44:28 -0300

    modelo do Exercício 6 foi definido.

---
+++ Lista01/src/br/cefetrj/mg/bsi/lista01/model/Exec06Model.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetrj.mg.bsi.lista01.model;
+
+/**
+ *
+ * @author cristian
+ */
+public class Exec06Model {
+    private String nome;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String sexo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+}
