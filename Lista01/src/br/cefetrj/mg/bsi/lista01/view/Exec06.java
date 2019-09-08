1b335dd147ae353d6bf48b359e66cd97ea7ff439
Cristian Madeira deSouza Pereira <cristianpereira@tic.ufrj.br>
2019-09-08 19:14:48 -0300

    Exemplo de uso da Utils no exercício 06.

--- Lista01/src/br/cefetrj/mg/bsi/lista01/view/Exec06.java
+++ Lista01/src/br/cefetrj/mg/bsi/lista01/view/Exec06.java
@@ -5,23 +5,30 @@
  */
 package br.cefetrj.mg.bsi.lista01.view;
 
+import br.cefetrj.mg.bsi.lista01.model.Exec06Model;
 import javax.swing.JOptionPane;
+import br.cefetrj.mg.bsi.lista01.utils.Utils;
 
 /**
  *
  * @author cristian
  */
-public class Exec06 {
+public class Exec06  extends Utils{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    private static final String TITLE="Cálculo de IMC";
+    
+    
+    public static void main(String[] args)  {
         // TODO code application logic here
         int resp = 0;
         do {
-            Exec06 pessoa = new Exec06();
-            resp = JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Cálculo de IMC", JOptionPane.YES_NO_OPTION);
+            Exec06Model pessoa = new Exec06Model();
+            String nome=input("Informe seu nome:",TITLE);
+            pessoa.setNome(nome);
+            resp = confirm("Deseja continuar?", TITLE);
         } while (resp == JOptionPane.YES_OPTION);
 
     }
