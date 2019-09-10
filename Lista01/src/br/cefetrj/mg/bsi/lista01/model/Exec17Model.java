98d00616836d782cfff3d50862f8bd9047288547
cristian <cristianmadeira1106@gmail.com>
2019-09-10 18:05:07 -0300

    View, DAO, Model do exercício 17 declarados.

---
+++ Lista01/src/br/cefetrj/mg/bsi/lista01/model/Exec17Model.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetrj.mg.bsi.lista01.model;
+
+/**
+ *
+ * @author cristian
+ */
+public class Exec17Model {
+    private String sexo;
+    private int idade;
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+}
