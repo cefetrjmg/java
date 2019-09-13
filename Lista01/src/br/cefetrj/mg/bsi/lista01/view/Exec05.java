604ebabf4dd414bd8f0c565044ea98785b1c9ecf
suely_moraes <suely.moraes@agencian1.com.br>
2019-09-13 14:32:03 -0300

    05,09,10,11

--- Lista01/src/br/cefetrj/mg/bsi/lista01/view/Exec05.java
+++ Lista01/src/br/cefetrj/mg/bsi/lista01/view/Exec05.java
@@ -1,9 +1,26 @@
 package br.cefetrj.mg.bsi.lista01.view;
 
-public class Exec05 {
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        
-    }
-    
+public class Test { 
+    public static void main(String[] args) { 
+        int meta; 
+        int valorVendas; 
+        String desempenho; 
+        int bonus; 
+        meta = Entrada.in.lerInt("Meta de vendas? "); 
+        valorVendas = Entrada.in.lerInt("Total de vendas realizadas? "); 
+        desempenho = "insatisfatorio"; 
+        bonus = 0; 
+        if(valorVendas >= 2 * meta) { 
+            desempenho = "excelente!"; 
+            bonus = valorVendas/10; 
+        } else if(valorVendas >= meta) { 
+            desempenho = "satisfatorio"; 
+            bonus = valorVendas/20; 
+        } 
+        System.out.println("Seu desempenho eh: " + desempenho); 
+        System.out.println("Seu bonus eh: " + bonus); 
+    } 
 }
